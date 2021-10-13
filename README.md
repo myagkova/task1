2486d9fa7337750083fba9a4cd27e899ce534e29
Angelina Myagkova <aa.myagkova@gmail.com>
2021-10-13 20:10:04 +0200

    add wondows option

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # task1
+
+По умолчанию данные выводятся в консоль. 
+Для сохранения в файл .csv нужно вызвать в терминале:
+python main.py --process /home/Telegram/Telegram --interval 10 > statistics.csv
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import getopt
+import os
 import subprocess
 import sys
 import time
@@ -11,19 +12,22 @@ def monitor_process_statistics(path, interval):
     p = subprocess.Popen(path, shell=True, stdout=subprocess.DEVNULL,
                          stderr=subprocess.DEVNULL)
     process = psutil.Process(p.pid)
-    print('time', 'cpu_usage', 'resident_set_size', 'virtual_memory_size',
-          'file_descriptor', sep=";")
+    if os.name == 'nt':
+        print('time', 'cpu_usage', 'working_set', 'private_bytes',
+              'handles', sep=";")
+    else:
+        print('time', 'cpu_usage', 'resident_set_size', 'virtual_memory_size',
+              'file_descriptors', sep=";")
     while True:
         if p.poll() is not None:
             return
         with process.oneshot():
             cpu_usage = process.cpu_percent()
-            memory_info = process.memory_full_info()
+            memory_info = process.memory_info()
             rss = memory_info.rss
             vms = memory_info.vms
-            num_fds = process.num_fds()
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(current_time, cpu_usage, rss, vms, num_fds, sep=";")
+            num_fds = process.num_handles() if os.name == 'nt' else process.num_fds()
+            print(datetime.now(), cpu_usage, rss, vms, num_fds, sep=";")
             time.sleep(interval)
 
 
